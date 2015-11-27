a301b0d069b5c00ea86280d4a4cbf4f01cdf6299
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-11-27 12:34:40 +0100

    primer commit

---
+++ Boletin11_1/src/boletin11_1/Coche.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package boletin11_1;
+
+/**
+ *
+ * @author jbarrosgarcia
+ */
+public class Coche {
+    private String matricula;
+
+    public Coche() {
+    }
+
+    public Coche(String matricula) {
+        this.matricula = matricula;
+    }
+    
+}
